1a9ed91e5c563a42b02fd9ac86d67f3fb2bc945d
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-02-28 17:15:14 -0600

    completed

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+var emailInput = document.querySelector("#email");
+var passwordInput = document.querySelector("#password");
+var signUpButton = document.querySelector("#sign-up");
+var msgDiv = document.querySelector("#msg");
+var userEmailSpan = document.querySelector("#user-email");
+var userPasswordSpan = document.querySelector("#user-password");
+
+renderLastRegistered();
+
+function displayMessage(type, message) {
+  msgDiv.textContent = message;
+  msgDiv.setAttribute("class", type);
+}
+
+function renderLastRegistered() {
+  // TODO: Retrieve the last email and password and render it to the page
+  if (localStorage.getItem("userEmail") && localStorage.getItem("userPassword")) {
+    userEmailSpan.textContent = localStorage.getItem("userEmail");
+    userPasswordSpan.textContent = localStorage.getItem("userPassword");
+} else {
+  displayMessage("error", "Please register first");
+}
+localStorage.getItem("last-Registered");
+}
+
+signUpButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var email = document.querySelector("#email").value;
+  var password = document.querySelector("#password").value;
+
+  if (email === "") {
+    displayMessage("error", "Email cannot be blank");
+  } else if (password === "") {
+    displayMessage("error", "Password cannot be blank");
+  } else {
+    displayMessage("success", "Registered successfully");
+
+  // TODO: Save email and password to localStorage and render the last registered user
+  localStorage.setItem("userEmail", email);
+  localStorage.setItem("userPassword", password);
+  renderLastRegistered();
+  }
+});
